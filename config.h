e43e44e779029c906a380d3f8e04a939cdb28ae5
RickSeiden <rick@seidenmail.com>
2024-02-28 07:51:57 -0500

    Initial Upload

---
+++ config.h
@@ -0,0 +1,24 @@
+// Copyright 2024 TODO (@TODO)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define ADNS5050_SCLK_PIN B5
+#define ADNS5050_SDIO_PIN C6
+#define ADNS5050_CS_PIN B4 
